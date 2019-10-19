096d5b244a3365b88c179640f9506ea3b16c0ba2
masato.uno <masato.u20@gmail.com>
2019-10-19 18:02:09 +0000

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resource :users, only: [:show, :new, :create]
+  
 end
---
+++ db/migrate/20191019165930_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email
+      t.string :password_digest
+      t.integer :sex, null: false, default: 1
+      t.date :bithday
+      t.boolean :administrator, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
